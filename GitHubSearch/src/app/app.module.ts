55965eb28f38deea5609acb5099a34d34ce8d7d5
kelvinhovinho <kelvinadamba@hotmail.com>
2021-02-11 19:45:17 +0300

    created Github-repo component

--- GitHubSearch/src/app/app.module.ts
+++ GitHubSearch/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GithubReposComponent } from './github-repos/github-repos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GithubReposComponent,
   ],
   imports: [
     BrowserModule,
