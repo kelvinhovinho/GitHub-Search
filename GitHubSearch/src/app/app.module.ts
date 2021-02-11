dafd58fbdf63d6e3c844565f89bd2411bf9adae6
kelvinhovinho <kelvinadamba@hotmail.com>
2021-02-11 19:46:20 +0300

    component Github-profile-data

--- GitHubSearch/src/app/app.module.ts
+++ GitHubSearch/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GithubReposComponent } from './github-repos/github-repos.component';
+import { GithubProfileDataComponent } from './github-profile-data/github-profile-data.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GithubReposComponent,
+    GithubProfileDataComponent,
   ],
   imports: [
     BrowserModule,
