7620561c6e583e601f7dccd001baf4184c0eb9cf
kelvinhovinho <kelvinadamba@hotmail.com>
2021-02-11 19:47:28 +0300

    Github-profile-card

--- GitHubSearch/src/app/app.module.ts
+++ GitHubSearch/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GithubReposComponent } from './github-repos/github-repos.component';
 import { GithubProfileDataComponent } from './github-profile-data/github-profile-data.component';
+import { GithubProfileCardComponent } from './github-profile-card/github-profile-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GithubReposComponent,
     GithubProfileDataComponent,
+    GithubProfileCardComponent,
   ],
   imports: [
     BrowserModule,
